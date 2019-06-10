29e754d692a705622c3db2436e8bf2ec8822c713
Luis del Cid <luisdelcid@gmail.com>
2019-06-10 09:53:42 -0600

    Create ldc-functions.php

---
+++ ldc-functions.php
@@ -0,0 +1,60 @@
+<?php
+
+function ldc_guid_to_postid($guid = ''){
+  if($guid){
+    global $wpdb;
+    $str = "SELECT ID FROM $wpdb->posts WHERE guid = %s";
+    $sql = $wpdb->prepare($str, $guid);
+    $post_id = $wpdb->get_var($sql);
+    if($post_id){
+      return (int) $post_id;
+    }
+  }
+  return 0;
+}
+
+function ldc_attachment_url_to_postid($url = ''){
+  if($url){
+    $post_id = ldc_guid_to_postid($url); // full size
+    if($post_id){
+      return $post_id;
+    }
+    preg_match('/^(.+)(-\d+x\d+)(\.' . substr($url, strrpos($url, '.') + 1) . ')?$/', $url, $matches);
+    if($matches){
+      $url = $matches[1];
+      if(isset($matches[3])){
+        $url .= $matches[3];
+      }
+    }
+    $post_id = ldc_guid_to_postid($url); // resized
+    if($post_id){
+      return $post_id;
+    }
+    preg_match('/^(.+)(-e\d+)(\.' . substr($url, strrpos($url, '.') + 1) . ')?$/', $url, $matches);
+    if($matches){
+      $url = $matches[1];
+      if(isset($matches[3])){
+        $url .= $matches[3];
+      }
+    }
+    $post_id = ldc_guid_to_postid($url); // edited
+    if($post_id){
+      return $post_id;
+    }
+  }
+  return 0;
+}
+
+function ldc_url_to_postid($url = ''){
+  if($url){
+    $post_id = url_to_postid($url);
+    if($post_id){
+      return $post_id;
+    }
+    $post_id = ldc_attachment_url_to_postid($url);
+    if($post_id){
+      return $post_id;
+    }
+  }
+  return 0;
+}
